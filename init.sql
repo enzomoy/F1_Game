d02ebf6fb9f271a0b7f87252c0e4aec8931c1106
Enzo <enzo.moy94@gmail.com>
2024-01-18 18:02:21 +0100

    WIP - Move drivers to db

--- init.sql
+++ init.sql
@@ -29,3 +29,32 @@ VALUES
     (1, 'Baku City Circuit', 'Azerbaïdjan', 51, 6.003, 306.049, 'Urbain', 3, 3, 12, 8, 2000, 2),
     (2, 'Silverstone Circuit', 'Royaume-Uni', 52, 5.891, 306.332, 'Plein air', 4, 5, 8, 10, 875, 2),
     (3, 'Circuit de Spa-Francorchamps', 'Belgique', 44, 7.004, 308.176, 'Montagne', 5, 3, 9, 11, 2015, 2);
+
+CREATE TABLE IF NOT EXISTS `f1_project`.`drivers` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(100) NOT NULL,
+    races_won INT DEFAULT 0,
+    team VARCHAR(50)
+);
+
+INSERT INTO drivers (full_name, team, races_won) VALUES
+    ('Lewis Hamilton', 'Mercedes', 8),
+    ('Valtteri Bottas', 'Mercedes', 1),
+    ('Max Verstappen', 'Red Bull Racing', 10),
+    ('Sergio Perez', 'Red Bull Racing', 1),
+    ('Lando Norris', 'McLaren', 0),
+    ('Daniel Ricciardo', 'McLaren', 1),
+    ('Charles Leclerc', 'Ferrari', 0),
+    ('Carlos Sainz', 'Ferrari', 0),
+    ('Pierre Gasly', 'AlphaTauri', 0),
+    ('Yuki Tsunoda', 'AlphaTauri', 0),
+    ('Fernando Alonso', 'Alpine', 0),
+    ('Esteban Ocon', 'Alpine', 1),
+    ('Sebastian Vettel', 'Aston Martin', 0),
+    ('Lance Stroll', 'Aston Martin', 0),
+    ('Kimi Raikkonen', 'Alfa Romeo', 0),
+    ('Antonio Giovinazzi', 'Alfa Romeo', 0),
+    ('Mick Schumacher', 'Haas', 0),
+    ('Nikita Mazepin', 'Haas', 0),
+    ('George Russell', 'Williams', 0),
+    ('Nicholas Latifi', 'Williams', 0);
