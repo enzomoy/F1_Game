38bce4710df1e9bffa2a85a6936efaae65c37f0d
Karimarf <karimaarfaouim@gmail.com>
2024-01-20 19:23:13 +0100

    reload

--- src/pilots_fonctions.c
+++ src/pilots_fonctions.c
@@ -6,26 +6,26 @@ char **getAllPilots() {
         pilots[i] = (char *)malloc(18 * sizeof(char));
     }
 
-    strcpy(pilots[0], "Lewis Hamilton");
-    strcpy(pilots[1], "Valtteri Bottas");
-    strcpy(pilots[2], "Max Verstappen");
+    strcpy(pilots[0], "Max Verstappen");
+    strcpy(pilots[1], "Lewis Hamilton");
+    strcpy(pilots[2], "Valtteri Bottas");
     strcpy(pilots[3], "Sergio Perez");
-    strcpy(pilots[4], "Lando Norris");
-    strcpy(pilots[5], "Daniel Ricciardo");
-    strcpy(pilots[6], "Charles Leclerc");
-    strcpy(pilots[7], "Carlos Sainz");
-    strcpy(pilots[8], "Pierre Gasly");
-    strcpy(pilots[9], "Yuki Tsunoda");
+    strcpy(pilots[4], "Daniel Ricciardo");
+    strcpy(pilots[5], "Esteban Ocon");
+    strcpy(pilots[6], "Carlos Sainz");
+    strcpy(pilots[7], "Lando Norris");
+    strcpy(pilots[8], "Charles Leclerc");
+    strcpy(pilots[9], "Pierre Gasly");
     strcpy(pilots[10], "Fernando Alonso");
-    strcpy(pilots[11], "Esteban Ocon");
-    strcpy(pilots[12], "Sebastian Vettel");
-    strcpy(pilots[13], "Lance Stroll");
-    strcpy(pilots[14], "Kimi Raikkonen");
-    strcpy(pilots[15], "Antonio Giovinazzi");
-    strcpy(pilots[16], "Mick Schumacher");
+    strcpy(pilots[11], "Sebastian Vettel");
+    strcpy(pilots[12], "Lance Stroll");
+    strcpy(pilots[13], "Yuki Tsunoda");
+    strcpy(pilots[14], "George Russell");
+    strcpy(pilots[15], "Nicholas Latifi");
+    strcpy(pilots[16], "Antonio Giovinazzi");
     strcpy(pilots[17], "Nikita Mazepin");
-    strcpy(pilots[18], "George Russell");
-    strcpy(pilots[19], "Nicholas Latifi");
+    strcpy(pilots[18], "Mick Schumacher");
+    strcpy(pilots[19], "Kimi Raikkonen");
 
     return pilots;
 }
